d7f123a173222cd78e75fc386be37595768e5b56
SB <sb@SBs-MacBook-Pro.local>
2018-12-01 11:52:20 -0500

    This is the TouchTracker app; added a custom view class and a custom struct type called DrawView and Line respectively, and used UIBezierPath instances to create and stroke a path

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by SB on 11/30/18.
+//  Copyright © 2018 SB. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
