09316e9dd545a2465d286433d97a132920d59ec7
SB <sb@SBs-MacBook-Pro.local>
2018-12-17 15:08:56 -0500

    Implemented tap and double tap recognition using UITapGestureRecognizer, double tap deletes all the lines drawn so far while tap on a line presents an instance of UIMenuController with a delete option

---
+++ TouchTracker/Circle.swift
@@ -0,0 +1,16 @@
+//
+//  Circle.swift
+//  TouchTracker
+//
+//  Created by SB on 12/1/18.
+//  Copyright © 2018 SB. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Circle {
+    
+    var firstCorner: CGFloat
+    var secondCorner: CGFloat
+}
--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -11,6 +11,14 @@ import UIKit
 class DrawView: UIView {
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
+    var selectedLineIndex: Int? {
+        didSet {
+            if selectedLineIndex == nil {
+                let menu = UIMenuController.shared
+                menu.setMenuVisible(false, animated: true)
+            }
+        }
+    }
     
     @IBInspectable var finishedLinesColor: UIColor = UIColor.black {
         didSet {
@@ -51,6 +59,12 @@ class DrawView: UIView {
         currentLinesColor.setStroke()
         for (_, line) in currentLines {
             stroke(line)
+        }
+        
+        if let index = selectedLineIndex {
+            UIColor.green.setStroke()
+            let selectedLine = finishedLines[index]
+            stroke(selectedLine)
         }
     }
     
@@ -118,4 +132,91 @@ class DrawView: UIView {
         setNeedsDisplay()
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawView.doubleTap(_:)))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.delaysTouchesBegan = true
+        addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawView.tap(_:)))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+    }
+    
+    @objc func doubleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a double tap")
+        
+        currentLines.removeAll()
+        finishedLines.removeAll()
+        selectedLineIndex = nil
+        setNeedsDisplay()
+    }
+    
+    @objc func tap(_ gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a tap!")
+        
+        let point = gestureRecognizer.location(in: self)
+        selectedLineIndex = indexOfLine(at: point)
+        
+        // Grab the menu controller
+        let menu = UIMenuController.shared
+        
+        if selectedLineIndex != nil {
+            // Make DrawView the target of menu item action messages
+            becomeFirstResponder()
+            
+            // Create a new "Delete" UIMenuItem
+            let deleteItem = UIMenuItem(title: "Delete", action: #selector(DrawView.deleteLine(_:)))
+            menu.menuItems = [deleteItem]
+            
+            // Tell the menu where it should come from and show it
+            let targetRect = CGRect(x: point.x, y: point.y, width: 2, height: 2)
+            menu.setTargetRect(targetRect, in: self)
+            menu.setMenuVisible(true, animated: true)
+        } else {
+            // Hide the menu if no line was selected
+            menu.setMenuVisible(false, animated: true)
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    func indexOfLine(at point: CGPoint) -> Int? {
+        // Find a line close to point
+        for (index, line) in finishedLines.enumerated() {
+            let begin = line.begin
+            let end = line.end
+            
+            // Check a few points on the line
+            for t in stride(from: CGFloat(0), to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                // If the tapped point is within 20 points, let's return this line
+                if hypot(x - point.x, y - point.y) < 20.0 {
+                    return index
+                }
+            }
+        }
+        // If nothing is close enough to the tapped point, then we did not select a line
+        return nil
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    @objc func deleteLine(_ sender: UIMenuController) {
+        // Remove the selected line from the list of finishedLines
+        if let index = selectedLineIndex {
+            finishedLines.remove(at: index)
+            selectedLineIndex = nil
+            
+            // Redraw
+            setNeedsDisplay()
+        }
+    }
 }
